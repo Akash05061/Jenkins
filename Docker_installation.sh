759befa4f2ef22a22d476676b14e7b304fb327c5
Akash Suttakoti <asuttakoti@gmail.com>
2025-09-06 11:14:47 +0530

    Update and rename Docker_installation.sh to Docker_installation

--- Docker_installation.sh
+++
@@ -1,8 +0,0 @@
-sudo apt-get update -y
-sudo apt-get install docker.io -y
-sleep 5
-sudo usermod -aG docker ubuntu 
-sudo usermod -aG docker jenkins 
-newgrp docker
-sleep 5
-sudo chmod 777 /var/run/docker.sock
